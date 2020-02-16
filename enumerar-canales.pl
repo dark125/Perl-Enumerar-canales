350e240d2444f47aa9c9acc098c38190dee9c3fc
dark125 <one1grafik@gmail.com>
2020-02-16 03:14:07 -0500

    Update enumerar-canales.pl

--- enumerar-canales.pl
+++ enumerar-canales.pl
@@ -1,10 +1,10 @@
-open FILE, "<lista-script-mexicano.m3u";
+open FILE, "<lista-script-mex-editado.m3u";
 @fichero = <FILE>;
 close FILE;
  
-my $i = 16;
+my $i = 0;
                                
-open FILE, ">lista-script-mexicano2.m3u";
+open FILE, ">lista-script-mex-editado-2.m3u";
 for (@fichero) {
     s/tvg-chno="[^"]*/$& . ++$i/ge;
     print FILE $_;
